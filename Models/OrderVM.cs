741968f54286fc91b00ba477302f18827ce34a8c
RayMasekela <62016197+RayMasekela@users.noreply.github.com>
2022-10-29 19:37:27 +0200

    Add files via upload

---
+++ Models/OrderVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW6.Models
+{
+    public class OrderVM
+    {
+        public DateTime orderdate { get; set; }
+        public string category { get; set; }
+        public int Id { get; set; }
+        public List<product> Products { get; set; }
+        public product Product { get; set; }
+        public decimal Total { get; set; }  
+        public int quantity { get; set; }
+        public decimal price { get; set; }   
+        
+
+    }
+}
